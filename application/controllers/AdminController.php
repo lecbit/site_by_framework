297941fcbb86ea2c1c98c8bb29fe1f0f9dbdda04
unknown <artm-marych@rambler.ru>
2020-10-01 16:17:05 +0200

    add_posts

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -5,25 +5,64 @@ use application\core\Controller;
 
 
 class AdminController extends Controller{
+
+    public function __construct($route) {
+		parent::__construct($route);
+        $this->view->layout = 'admin';
+
+    }
     
     public function loginAction(){
+        if(isset($_SESSION['admin'])){
+            $this->view->redirect('admin/add');
+        }
+        if(!empty($_POST)){
+            if(!$this->model->loginValidate($_POST)){
+                $this->view->message('error', $this->model->error);
+            }
+            $_SESSION['admin'] = true;
+            $this->view->location('admin/add');
+        }
         $this->view->render('Вход');
     }
 
     public function addAction(){
+        if(!empty($_POST)){
+            if(!$this->model->postValidate($_POST, 'add')){
+                $this->view->message('error', $this->model->error);
+            }
+            $id = $this->model->postAdd($_POST);
+            if(!$id){
+                $this->view->message('success', 'Ошибка обработки запроса');
+            }
+            $this->model->postUploadImage($_FILES['img']['tmp_name'], $id);
+            $this->view->message('success', 'Пост добавлен');
+        }
         $this->view->render('Добавить пост');
     }
 
     public function editAction(){
+        if(!empty($_POST)){
+            if(!$this->model->postValidate($_POST, 'edit')){
+                $this->view->message('error', $this->model->error);
+            }
+            $this->view->message('success', 'ok');
+        }
         $this->view->render('Редактировать пост');
     }
 
     public function deleteAction(){
+        debug($this->route['id']);
         exit('Удаление');
     }
 
-    public function postAction(){
-        exit('Выход');
+    public function logoutAction(){
+        unset($_SESSION['admin']);
+        $this->view->redirect('admin/login');
+    }
+
+    public function postsAction(){
+        $this->view->render('Посты');
     }
 }
 
--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -15,6 +15,13 @@ class MainController extends Controller{
     }
 
     public function contactAction(){
+        if(!empty($_POST)){
+            if(!$this->model->contactValidate($_POST)){
+                $this->view->message('error', $this->model->error);
+            }
+            //mail('artm-marych@rambler.ru', 'Сообщение из блога', $_POST['name'].'|'.$_POST['email'].'|'.$_POST['text']);
+            $this->view->message('success', 'Форма работает');
+        }
         $this->view->render('Контакты');
     }
 
